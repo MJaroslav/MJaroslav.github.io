58d5ebe69a33f6aee59ae1fecc81712f788ecd2e
Yaroslav Novitsky <rcr3211@gmail.com>
2022-11-26 04:28:17 +0300

    refactor: rename communications to profiles; add: music WIP;

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,27 @@
 onLoad()
 
 function onLoad() {
-    createContacts()
+    createProfiles()
+    createMusic()
     enableExtraBootstrapTooltips()
 }
 
-function createContacts() {
-    $.get("data/contacts.json", function (json) {
+function createProfiles() {
+    $.get("data/profiles.json", function (json) {
         json.reverse().forEach(data => {
             var contactRoot = document.createElement("div")
-            contactRoot.className = "col"
+            contactRoot.className = "col profile-entry"
             var label = document.createElement("h5")
-            label.className = "d-inline-flex pt-2 pl-3"
+            label.className = "profile-entry-label d-inline-flex pt-2 pl-3"
             var icon = document.createElement("i")
             icon.className = data["icon"] + " mr-2"
             label.appendChild(icon)
             label.innerHTML += data["name"]
             if (data.hasOwnProperty("tooltip")) {
-                label.attributes += { "data-toggle": "toolptip", "data-html": "true" }
+                label.setAttribute("data-toggle", "tooltip")
+                label.setAttribute("data-html", "true")
                 label.title = data["tooltip"]
-                $(label).tooltip()
+                $(label).tooltip() // Fuck this fucking JS
             }
             if (data.hasOwnProperty("url")) {
                 var link = document.createElement("a")
@@ -27,7 +29,29 @@ function createContacts() {
                 link.appendChild(label)
                 contactRoot.appendChild(link)
             } else contactRoot.appendChild(label)
-            $(".contacts").prepend(contactRoot)
+            $(".profiles").prepend(contactRoot)
+        })
+    }, "json")
+}
+
+function createMusic() {
+    $.get("data/music.json", function (json) {
+        json.reverse().forEach(data => {
+            var musicRoot = document.createElement("a")
+            musicRoot.className = "music-entry p-3" // remove "p-3" for square images
+            musicRoot.href = data["url"]
+            var img = document.createElement("img")
+            img.src = "media/music/" + data["image"]
+            img.alt = data["name"]
+            img.className = "col music-entry-image p-0 rounded-circle border" // remove "rounded-circle border" for square images 
+            var tooltip = data["name"]
+            if (data.hasOwnProperty("tooltip")) tooltip += "<br><br>" + data["tooltip"]
+            musicRoot.setAttribute("data-toggle", "tooltip")
+            musicRoot.setAttribute("data-html", "true")
+            musicRoot.title = tooltip
+            $(musicRoot).tooltip() // Fuck this fucking JS
+            musicRoot.appendChild(img)
+            $(".music").prepend(musicRoot)
         })
     }, "json")
 }
