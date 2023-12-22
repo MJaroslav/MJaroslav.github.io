6079d405b7a13de9b1bfa11a02703279e6d76b04
MJaroslav <rcr3211@gmail.com>
2023-12-22 18:00:54 +0000

    Deploying to gh-pages from @ MJaroslav/MJaroslav.github.io@12a7075370e5e78efe1aabfbf3988d69eaccdd55 🚀

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,18 @@ onLoad()
 
 function onLoad() {
     enableExtraBootstrapTooltips()
+    enableHeaderScrollDown()
 }
 
 function enableExtraBootstrapTooltips() {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
+    })
+}
+
+function enableHeaderScrollDown() {
+    $('#header-scroll-down').click(function() {
+        var target = $('header').outerHeight();
+        $('html, body').animate({ scrollTop: target }, 400);
     })
 }
